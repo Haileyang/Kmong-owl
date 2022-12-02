9d30f13154fbb802ca1b02a2f38d4eb4c744c353
hailey <yanghyeri73@gmail.com>
2022-12-02 12:16:49 -0800

    Add 3 detailList pages & Finalize sub page draft

--- public/js/common.js
+++ public/js/common.js
@@ -11,17 +11,31 @@ function dropdownOpener(){
     
 }
 
-const navList = document.querySelectorAll('.nav-list')
-for (let i=0; i<navList.length; i++){
-    navList[i].addEventListener('mouseover', function(){
-        navList[i].classList.add('hover')
-        console.log('hi')
-    })
-    navList[i].addEventListener('mouseout', function(){
-        navList[i].classList.remove('hover')
-        console.log('hi')
-    })
-}
 function headerNavModifier(){
+    const navList = document.querySelectorAll('.nav-list')
+    for (let i=0; i<navList.length; i++){
+        navList[i].addEventListener('mouseover', function(){
+            navList[i].classList.add('hover')
+            console.log('hi')
+        })
+        navList[i].addEventListener('mouseout', function(){
+            navList[i].classList.remove('hover')
+            console.log('hi')
+        })
+    }
 }
 headerNavModifier()
+
+function textareaHandler (){
+    let textArea = document.getElementById("textbox");
+    let characterCounter = document.getElementById("char_count");
+    const maxNumOfChars = 200;
+    const countCharacters = () => {
+        let numOfEnteredChars = textArea.value.length;
+        let counter = numOfEnteredChars;
+        characterCounter.textContent = counter + "/200";
+    };
+    
+    textArea.addEventListener("input", countCharacters);
+}
+textareaHandler()
